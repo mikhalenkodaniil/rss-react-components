cd834370a98bed974f4cb4c10823c20aced74d53
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-10-29 17:25:05 +0100

    feat: add functionality search section

--- react-components/src/components/search/Search.tsx
+++ react-components/src/components/search/Search.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import SearchBtn from '../searchBtn/SearchBtn';
-import SearchInput from '../searchInput/SearchInput';
 import './search.css';
-
 class Search extends React.Component {
+  state = {
+    name: localStorage.getItem('name') ? localStorage.getItem('name') : ''
+  };
   render() {
+    const name = this.state.name ? this.state.name : '';
+    function storageSetItem() {
+      localStorage.setItem('name', name);
+    }
     return (
       <>
         <section className="search__section">
-          <SearchInput />
-          <SearchBtn />
+          <input
+            onChange={(e) => this.setState({ name: e.target.value })}
+            value={name}
+            className="search__inp"
+            placeholder="Search your people in StarWars"
+          ></input>
+          <button className="search__btn" onClick={storageSetItem}>
+            Search
+          </button>
         </section>
       </>
     );
