6798f1db6bb76e112a1f9015918a344c6ad8c530
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-11-02 16:37:07 +0100

    feat: migrate results for functional components

--- react-components/src/components/card/Card.tsx
+++ react-components/src/components/card/Card.tsx
@@ -1,33 +1,13 @@
-import React from 'react';
 import './card.css';
 interface CardProps {
-  name?: string;
-  height?: string;
-  mass?: string;
-  hair_color?: string;
-  skin_color?: string;
-  eye_color?: string;
-  gender?: string;
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  gender: string;
 }
-// class Card extends React.Component {
-//   render() {
-//     const data: CardProps = this.props.data;
-//     return (
-//       <>
-//         <section className="card">
-//           <div>Name: {data.name}</div>
-//           <div>Height: {data.height}</div>
-//           <div>Mass: {data.mass}</div>
-//           <div>Hair color: {data.hair_color}</div>
-//           <div>Skin color: {data.skin_color}</div>
-//           <div>Eye color: {data.eye_color}</div>
-//           <div>Gender: {data.gender}</div>
-//         </section>
-//       </>
-//     );
-//   }
-// }
-
 function Card({
   name,
   height,
@@ -37,6 +17,7 @@ function Card({
   eye_color,
   gender,
 }: CardProps) {
+  // console.log(props)
   return (
     <>
       <section className="card">
--- react-components/src/components/results/Results.tsx
+++ react-components/src/components/results/Results.tsx
@@ -1,68 +1,64 @@
-// import React from 'react';
-// import Card from '../card/Card';
+import Card from '../card/Card';
+import { useState } from 'react';
 import './results.css';
-// interface CardProps {
-//   name?: string;
-//   height?: string;
-//   mass?: string;
-//   hair_color?: string;
-//   skin_color?: string;
-//   eye_color?: string;
-//   gender?: string;
-// }
-// class Results extends React.Component {
-//   state = {
-//     data: [],
-//     loading: true,
-//     name: localStorage.getItem('name') ? localStorage.getItem('name') : '',
-//   };
 
-//   componentDidMount() {
-//     const url = 'https://swapi.dev/api/people/';
-//     fetch(url)
-//       .then((res) => res.json())
-//       .then((APIdata) => {
-//         const newData: CardProps = APIdata.results;
-//         this.setState({
-//           data: newData,
-//           loading: false,
-//           name: localStorage.getItem('name')
-//             ? localStorage.getItem('name')
-//             : '',
-//         });
-//         console.log(newData);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//         this.setState({ loading: false });
-//       });
-//   }
-//   render() {
-//     const { data, loading, name } = this.state;
-//     this.setState({
-//       data: data,
-//       loading: loading,
-//       name: localStorage.getItem('name') ? localStorage.getItem('name') : '',
-//     });
-//     if (loading) {
-//       return <div className="searching">Searching...</div>;
-//     }
-//     const out = data.map((el, id) => {
-//       if (!el.name.includes(name) && name !== '') return;
-//       return (
-//         <div key={id}>
-//           <Card data={el} />
-//         </div>
-//       );
-//     });
+interface IData {
+  results: IResults[];
+}
+interface IResults {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  gender: string;
+}
 
-//     return <section className="results__section">{out}</section>;
-//   }
-// }
+function Results() {
+  const [name, setName] = useState(
+    localStorage.getItem('name') ? localStorage.getItem('name') : ''
+  );
+  if (name != localStorage.getItem('name')) {
+    setName(localStorage.getItem('name') ? localStorage.getItem('name') : '');
+    console.log(name);
+  }
+  const [data, setData] = useState<IResults[]>([]);
+  const url = 'https://swapi.dev/api/people/';
+  fetch(url)
+    .then((res) => res.json())
+    .then((APIdata: IData) => {
+      if (!data[0]) {
+        setData(APIdata.results);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
-function Results () {
+  if (!data[0]) {
+    return <div className="searching">Searching...</div>;
+  }
+  const out = data.map((el: IResults, id: number) => {
+    if (!el.name.includes(name ? name : '') && name !== '') return;
+    return (
+      <div key={id}>
+        <Card
+          name={el.name}
+          height={el.height}
+          mass={el.mass}
+          hair_color={el.hair_color}
+          skin_color={el.skin_color}
+          eye_color={el.eye_color}
+          gender={el.gender}
+        />
+      </div>
+    );
+  });
   return (
-    <></>
+    <>
+      <section className="results__section">{out}</section>
+    </>
   );
 }
 
