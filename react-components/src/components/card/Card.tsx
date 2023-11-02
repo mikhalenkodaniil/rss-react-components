ff8d568a37072257cd0bd6400e0fadaec9ac7381
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-11-02 13:57:57 +0100

    feat:migration first sect for functional component

--- react-components/src/components/card/Card.tsx
+++ react-components/src/components/card/Card.tsx
@@ -9,23 +9,47 @@ interface CardProps {
   eye_color?: string;
   gender?: string;
 }
-class Card extends React.Component {
-  render() {
-    const data: CardProps = this.props.data;
-    return (
-      <>
-        <section className="card">
-          <div>Name: {data.name}</div>
-          <div>Height: {data.height}</div>
-          <div>Mass: {data.mass}</div>
-          <div>Hair color: {data.hair_color}</div>
-          <div>Skin color: {data.skin_color}</div>
-          <div>Eye color: {data.eye_color}</div>
-          <div>Gender: {data.gender}</div>
-        </section>
-      </>
-    );
-  }
+// class Card extends React.Component {
+//   render() {
+//     const data: CardProps = this.props.data;
+//     return (
+//       <>
+//         <section className="card">
+//           <div>Name: {data.name}</div>
+//           <div>Height: {data.height}</div>
+//           <div>Mass: {data.mass}</div>
+//           <div>Hair color: {data.hair_color}</div>
+//           <div>Skin color: {data.skin_color}</div>
+//           <div>Eye color: {data.eye_color}</div>
+//           <div>Gender: {data.gender}</div>
+//         </section>
+//       </>
+//     );
+//   }
+// }
+
+function Card({
+  name,
+  height,
+  mass,
+  hair_color,
+  skin_color,
+  eye_color,
+  gender,
+}: CardProps) {
+  return (
+    <>
+      <section className="card">
+        <div>Name: {name}</div>
+        <div>Height: {height}</div>
+        <div>Mass: {mass}</div>
+        <div>Hair color: {hair_color}</div>
+        <div>Skin color: {skin_color}</div>
+        <div>Eye color: {eye_color}</div>
+        <div>Gender: {gender}</div>
+      </section>
+    </>
+  );
 }
 
 export default Card;
