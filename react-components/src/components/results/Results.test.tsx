6a5e1d2aec8c0df67a8818dcf1f7ba6d3bdd110b
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-11-12 22:35:35 +0100

    feat: add test loading indicator is displayed

---
+++ react-components/src/components/results/Results.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import App from '../app/App';
+import { act } from 'react-dom/test-utils';
+
+act(() => {
+    test('renders learn react link', () => {
+        global.fetch = jest.fn(() =>
+          Promise.resolve({
+            json: () => Promise.resolve({ test: 100 }),
+          })
+        ) as jest.Mock;
+        render(<App />);
+        const linkElement = screen.getByText(/Searching.../i);
+        expect(linkElement).toBeInTheDocument();
+      });
+  });
