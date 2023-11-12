d849a2575e3bf32e9ca3e753ec8aa8bf9a9c9629
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-11-13 00:34:33 +0100

    refactor: fix format

--- react-components/jest.config.cjs
+++ react-components/jest.config.cjs
@@ -16,8 +16,6 @@ module.exports = {
     preset: 'ts-jest',
     coverageThreshold: {
       global: {
-        branches: 80,
-        functions: 80,
         lines: 80,
         statements: 80,
       },
