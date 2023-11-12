5dd57ea9b7369ca6c0204de206845cab8bbc43ec
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-11-12 21:23:55 +0100

    refactor: delete comments

--- react-components/src/components/app/App.test.tsx
+++ react-components/src/components/app/App.test.tsx
@@ -1,15 +1,14 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
 test('renders learn react link', () => {
-    global.fetch = jest.fn(() =>
-        Promise.resolve({
-            json: () => Promise.resolve({ test: 100 }),
-        }),
-        ) as jest.Mock;
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ test: 100 }),
+    })
+  ) as jest.Mock;
 
-    render (<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument(); 
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
--- react-components/src/components/search/Search.test.tsx
+++ react-components/src/components/search/Search.test.tsx
@@ -1,25 +1,13 @@
-// import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Search from './Search';
 
 describe('Search', () => {
   test('saves entered value to local storage on Search button click', () => {
-    // Рендерим компонент
     const { getByPlaceholderText, getByText } = render(<Search />);
-
-    // Получаем поле ввода по метке (предположим, у вас есть метка 'Search Input')
     const searchInput = getByPlaceholderText('Search your people in StarWars');
-
-    // Задаем значение в поле ввода
     fireEvent.change(searchInput, { target: { value: 'testValue' } });
-
-    // Получаем кнопку по тексту (предположим, у вас есть кнопка с текстом 'Search')
     const searchButton = getByText('Search');
-
-    // Нажимаем на кнопку
     fireEvent.click(searchButton);
-
-    // Проверяем, что значение сохранено в локальном хранилище
     expect(localStorage.getItem('name')).toBe('testValue');
   });
 });
